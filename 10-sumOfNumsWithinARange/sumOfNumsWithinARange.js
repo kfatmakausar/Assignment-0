009ee8cd0b4bbaf1b67d1d65e98e1f70f3ac685a
Fatma Kausar <kfatmakausar@gmail.com>
2020-05-28 13:49:48 -0400

    updated solution

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,11 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+  var count =0;
+  for (var i =0; i <nums.length; i++) {
+    if(nums[i] <= end && nums[i] >=start) {
+      count++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
