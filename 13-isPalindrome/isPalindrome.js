f375a399cae170d5f36976ff382a85b1e65543ae
Fatma Kausar <kfatmakausar@gmail.com>
2020-05-28 13:51:32 -0400

    updated solution

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,19 @@
 function isPalindrome(word) {
-  // Insert code here;
-}
+  word = word.toLowerCase();
+  var result= true;
+  var i = 0;
+  while (result != false && i<word.length){
+      if (word.charAt(i) === word.charAt(word.length-1-i)) {
+          result = true;
+      }
+      else {
+        result = false;
+      }
+      i++;
+    }
+    return result;
+  }
+  
 
 // Do not edit this line;
 module.exports = isPalindrome;
