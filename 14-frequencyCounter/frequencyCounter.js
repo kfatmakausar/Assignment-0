44ed3119bb961a90e8ed4d8374f56d05b2a53e71
Fatma Kausar <kfatmakausar@gmail.com>
2020-05-28 13:51:46 -0400

    updated solution

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,17 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  var frequencyCounter = {};  //create empty object
+
+  //loop through every letter in the string
+  for (var i =0; i < word.length; i++) {
+    if (frequencyCounter[word[i]]) {
+      frequencyCounter[word[i]] +=1;
+    }
+    else {
+      frequencyCounter[word[i]] =1;
+    }
+  }
+  return frequencyCounter;
+
 }
 
 // Do not edit this line;
